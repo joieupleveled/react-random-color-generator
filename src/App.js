68801ae1899bbc11c84823996e282d89fed43c38
joieupleveled <joieupleveled@mailfence.com>
2022-09-12 22:06:57 +0200

    Update project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import './App.css';
-import nodeColor from 'kuler';
+import { getByDisplayValue } from '@testing-library/react';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 import logo from './logo.svg';
 
 function App() {
-  const [color, setColor] = useState(nodeColor.random().color);
+  const [color, setColor] = useState('');
 
   return (
     <div>
-      <h1>Random Color Generator</h1>;
+      <div
+        style={{
+          margin: '0 auto',
+          display: 'flex',
+          justifycontent: 'center',
+          alignItems: 'center',
+          fontSize: 30,
+          border: '1px solid',
+        }}
+      >
+        <h1>Random Color Generator</h1>;
+      </div>
+
+      <button
+        onClick={() => setColor(randomColor())}
+        style={{
+          backgroundColor: color,
+          margin: '0 auto',
+          display: 'flex',
+          justifycontent: 'center',
+          alignItems: 'center',
+          fontSize: 100,
+          border: '10px solid',
+        }}
+      >
+        Guess the next color 😏
+      </button>
     </div>
   );
 }
 
-export defautl App;
+export default App;
