37489ba07036311c28c9a0f4c3dfd3d667eefcdd
joieupleveled <joieupleveled@mailfence.com>
2022-09-16 19:53:20 +0200

    Update project

--- src/App.js
+++ src/App.js
@@ -7,32 +7,36 @@ function App() {
   const [color, setColor] = useState('');
 
   return (
-    <div
-      className="generated"
-      style={{
-        width: 600,
-        height: 200,
-        fontSize: 10,
-        borderRadius: 10,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: '0 auto',
-        color: 'white',
-        backgroundColor: color,
-      }}
-    >
-      Generated Color: {color}
-      <div>
-        <button
-          onClick={() => {
-            setColor(randomColor);
-          }}
-        >
-          Generate
-        </button>
+    <>
+      <h1>Random Color Generator</h1>
+      <div
+        className="generated"
+        style={{
+          width: 600,
+          height: 200,
+          fontSize: 10,
+          borderRadius: 10,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          margin: '0 auto',
+          color: 'white',
+          backgroundColor: color,
+        }}
+      >
+        Generated Color: {color}
+        <div />
+        <div>
+          <button
+            onClick={() => {
+              setColor(randomColor);
+            }}
+          >
+            Generate
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
